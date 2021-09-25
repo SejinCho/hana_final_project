a0d53b8d29ee802b7bb3263652704338a7c332cd
SejinCho <whtpwls777@naver.com>
2021-09-26 08:33:40 +0900

    0926 commit

---
+++ bank-api/src/main/java/kr/ac/kopo/test/WebSocketTestController.java
@@ -0,0 +1,9 @@
+package kr.ac.kopo.test;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebSocketTestController {
+	
+}
